7cc5e5407ce760ae6acc5db8c5c87959fcca977d
Josh MacDonald <rockiesgrizzly@icloud.com>
2024-10-30 20:12:47 -0600

    inline doc

--- Tests/UseCaseTests.swift
+++ Tests/UseCaseTests.swift
@@ -11,14 +11,14 @@ import Testing
 @testable import Alarms
 
 final class UseCaseTests {
-    @Test("Test Alarms View Model can add a timer from an Alarm Model")
+    @Test("Test Alarms View Model adds a timer from an Alarm Model")
     func addingAlarmModelProducesTimer() async throws {
         let addedAlarmModel = AlarmModel(id: UUID(), date: AlarmModel.mockFutureDate, saved: true, sound: .party, recurrence: .weekly)
         let timer = UseCase_TimerFromAlarmModel.dispatchSourceTimer(for: addedAlarmModel)
         #expect(timer != nil)
     }
     
-    @Test("Test Alarms View Model can add a timer from an Alarm Model")
+    @Test("Test Date & Recurrence produce Date Components")
     func dateAndRecurrenceProducesComponents() async throws {
         let dateComponents = UseCase_DateComponentsForRecurrence.dateComponents(for: AlarmModel.mockFutureDate, and: .weekly)
         #expect(dateComponents != nil)
