c411d7a682a78b9dadab5c63f719c57989009557
Josh MacDonald <rockiesgrizzly@icloud.com>
2024-10-31 10:26:58 -0600

    zero seconds timer

--- Domain/UseCase_TimerFromAlarmModel.swift
+++ Domain/UseCase_TimerFromAlarmModel.swift
@@ -7,17 +7,27 @@ protocol UseCase_TimerFromAlarmModelProtocol {
 /// Cancels system notification connected to an alarm if user enabled notifications
 struct UseCase_TimerFromAlarmModel: UseCase_TimerFromAlarmModelProtocol {
     static func dispatchSourceTimer(for alarmModel: AlarmModel) -> DispatchSourceTimer? {
-        guard alarmModel.date > Date() else { return nil }
+        // Extract all components but seconds since the user didn't set these
+        let componentsSansSeconds = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute],
+                                                         from: alarmModel.date)
         
+        guard alarmModel.date > Date(),
+              // Start at :00 seconds of the chosen minute since we don't offer seconds
+              let dateZeroSeconds = Calendar.current.date(from: componentsSansSeconds) else { return nil }
+
+        // Adjust the interval
+        let dateInterval = dateZeroSeconds.timeIntervalSinceNow
+
         let timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
-        let interval = alarmModel.date.timeIntervalSinceNow
-        timer.schedule(deadline: .now() + interval)
-        
+                        
+        timer.schedule(deadline: .now() + dateInterval)
+
         timer.setEventHandler {
             Task { @MainActor in
                 AlarmAlertPresenter.trigger(with: alarmModel)
             }
         }
+
         timer.resume()
         return timer
     }
